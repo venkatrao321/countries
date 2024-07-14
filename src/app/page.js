84010f28fdf2ab08b18dacd92da2f9b238119bfa
@venkatrao156 <venkatrao321@gmail.com>
2024-07-15 00:55:15 +0530

    added error

--- src/app/page.js
+++ src/app/page.js
@@ -2,20 +2,18 @@
 import axios from "axios";
 import { useEffect,useState} from "react";
 export default function Home() {
-    const [contryFlags,setcontryFlags]=useState([]);
-    useEffect(()=>{
-      try {
-        const url="https://xcountries-backend.azurewebsites.net/all"
-      axios.get(url).then((response)=>{
-        console.log(response)
-        setcontryFlags(response.data)
-      })
-      } catch (error) {
-        console.log(error)
-      }
-      
-       
-    })
+    const [contryFlags,setCountryFlags]=useState([]);
+    useEffect(() => {
+      const url = "https://xcountries-backend.azurewebsites.net/all";
+      axios.get(url)
+        .then((response) => {
+          console.log(response);
+          setCountryFlags(response.data);
+        })
+        .catch((error) => {
+          console.error('Error fetching the data:', error);
+        });
+    }, []);
   return (
     <main className="flex min-h-screen items-center flex-wrap justify-between p-24">
     {contryFlags?.map((data)=>{
